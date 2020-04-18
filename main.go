6116b2fbf0bca9e0be47050de11f2278de698e54
asvvvad <36175183+asvvvad@users.noreply.github.com>
2020-04-18 06:26:33 +0100

    v0.2
    - Removed climenu dependency which also dependency on a module for terminal manipulation that only works on Linux
    - Use flag for parsing flags in recommendation of [c0m3tx](https://www.reddit.com/user/c0m3tx/)'s [comment](https://www.reddit.com/r/golang/comments/g2vh9j/search_and_copy_lyrics_from_the_terminal_first_go/fnqi248)
    This update 
    - Use environment new variable for getting the token to ease the installation thanks to [_noobAtLife__](https://www.reddit.com/user/_noobAtLife_/)'s [comment](https://www.reddit.com/r/golang/comments/g2vh9j/search_and_copy_lyrics_from_the_terminal_first_go/fno9i8k)
    - Some minor code cleaning and output color changes

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/tsirysndr/go-genius"
-	lyrics "github.com/bigheadgeorge/lyrics/genius"
-	"github.com/paulrademacher/climenu"
-	"github.com/wzshiming/ctc"
 	"github.com/atotto/clipboard"
+	lyrics "github.com/bigheadgeorge/lyrics/genius"
+	"github.com/tsirysndr/go-genius"
+	"github.com/wzshiming/ctc"
 	"os"
 	"strings"
-	"strconv"
-	"regexp"
+	"flag"
 )
 
 func err(error string, exit bool) {
@@ -20,39 +18,54 @@ func err(error string, exit bool) {
 	}
 }
 
+func info(info string) {
+	fmt.Println(ctc.ForegroundBrightYellow, "+ "+info+"\n", ctc.Reset)
+}
+
+func success(text string) {
+	fmt.Println(ctc.ForegroundGreen, "* "+text+"\n", ctc.Reset)
+}
+
 func main() {
-	help := fmt.Sprint(ctc.ForegroundBrightCyan, "CPLY:", ctc.Reset, " Get lyrics copied to your clipboard.\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Usage:", ctc.Reset, " cply [options] song title..\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Options:", ctc.Reset, "\n\n    ", ctc.ForegroundBrightGreen, "-h, --help:", ctc.Reset, "     Print this help text and exit.\n    ", ctc.ForegroundBrightGreen, "-1, --first:", ctc.Reset, "    Do not prompt to choose a song but select the first result.\n    ", ctc.ForegroundBrightGreen, "-p, --print:", ctc.Reset, "    Print the lyrics not just copy them.\n    ", ctc.ForegroundBrightGreen, "-n, --no-color:", ctc.Reset, "    Don't use colors when printing lyrics.","\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Environment Variables:", ctc.Reset, "\n\n    ",ctc.ForegroundBrightGreen, "CPLY_FIRST", ctc.Reset,ctc.ForegroundBrightGreen, "CPLY_PRINT", ctc.Reset,ctc.ForegroundBrightGreen, "CPLY_NOCOLOR", ctc.Reset,"\n\nCPLY 0.1 by ASVVVAD (", ctc.ForegroundBrightCyan, "https://asvvvad.eu.org", ctc.Reset, ") | Github: ", ctc.ForegroundBrightCyan, "(https://github.com/asvvvad/cply)", ctc.Reset)
 
+	// Get the access_token from the environment to ease the install process
+	// suggested by https://www.reddit.com/user/_noobAtLife_/ at https://www.reddit.com/r/golang/comments/g2vh9j/search_and_copy_lyrics_from_the_terminal_first_go/fno9i8k
+	if os.Getenv("CPLY_TOKEN") == "" {
+		err("Please provide an access token (from https://genius.com/api-clients) in your environment as $CPLY_TOKEN", true)
+	}
 
-	if len(os.Args) == 1 {
+	help := fmt.Sprint(ctc.ForegroundBrightCyan, "CPLY:", ctc.Reset, " Get lyrics copied to your clipboard.\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Usage:", ctc.Reset, " cply [options] song title..\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Options:", ctc.Reset, "\n\n    ", ctc.ForegroundBrightCyan, "-h, -help:", ctc.Reset, "     Print this help text and exit.\n    ", ctc.ForegroundBrightCyan, "-1, -first:", ctc.Reset, "    Do not prompt to choose a song but select the first result.\n    ", ctc.ForegroundBrightCyan, "-p, -print:", ctc.Reset, "    Print the lyrics not just copy them.\n    ", ctc.ForegroundBrightCyan, "-n, -no-color:", ctc.Reset, "    Don't use colors when printing lyrics.", "\n\n", ctc.ForegroundBrightGreen|ctc.Underline, "Environment Variables:", ctc.Reset, "\n\n    ", ctc.ForegroundBrightCyan, "CPLY_TOKEN", ctc.Reset, ctc.ForegroundBrightCyan, "CPLY_FIRST", ctc.Reset, ctc.ForegroundBrightCyan, "CPLY_PRINT", ctc.Reset, ctc.ForegroundBrightCyan, "CPLY_NOCOLOR", ctc.Reset, "\n\nCPLY v0.2 by ASVVVAD (", ctc.ForegroundBrightCyan, "https://asvvvad.eu.org", ctc.Reset, ") | Github: ", ctc.ForegroundBrightCyan, "(https://github.com/asvvvad/cply)", ctc.Reset)
+	// parse flags
+	var chooseFirst bool
+	flag.BoolVar(&chooseFirst, "1", false, "")
+	flag.BoolVar(&chooseFirst, "first", false, "")
+	var printLyrics bool
+	flag.BoolVar(&printLyrics, "p", false, "")
+	flag.BoolVar(&printLyrics, "print", false, "")
+	var noColors bool
+	flag.BoolVar(&noColors, "n", false, "")
+	flag.BoolVar(&noColors, "no-color", false, "")
+	var showHelp bool
+	flag.BoolVar(&showHelp, "h", false, "")
+	flag.BoolVar(&showHelp, "help", false, "")
+
+	flag.Parse()
+
+	if showHelp {
+		fmt.Println(help)
+		os.Exit(0)
+	}
+
+	if len(flag.Args()) == 0 {
 		err("Song not specified specified\n", false)
 		fmt.Println(help)
 		os.Exit(1)
 	}
 
-	chooseFirst := false
-	printLyrics := false
-	noColors := false
-	q := ""
-	// parse flags	
+	// the search query
+	var q string
 
-	for i := range os.Args {
-		switch os.Args[i] {
-			case "-1", "--first":
-				chooseFirst = true
-			case "-h", "--help":
-				fmt.Println(help)
-				os.Exit(0)
-			case "-p", "--print":
-				printLyrics = true
-			case "-n", "--no-color":
-				noColors = true
-		}
-	}
-
-	r, _ := regexp.Compile("(-p|--print|-h|--help|-1|--first|./|cply|-n|--no-color)")
-
-	q = r.ReplaceAllString(strings.Join(os.Args, ""), "")
+	q = strings.TrimSpace(strings.Join(flag.Args(), " "))
 
 	if q == "" {
 		err("Song not specified specified\n", false)
@@ -71,60 +84,72 @@ func main() {
 		chooseFirst = true
 	}
 
-	token := "access_token"
+
+	token := os.Getenv("CPLY_TOKEN")
 	client := genius.NewClient(token)
 	res, _ := client.Search.Get(q)
 
 	if len(res) == 0 {
-		fmt.Println(ctc.ForegroundRed, "No results found", ctc.Reset)
+		err("No results found, try again.", true)
 		os.Exit(1)
 	}
 
-	url := ""
-	title := ""
+	var url string
+	var title string
 
 	if chooseFirst {
 		url = res[0].Result.URL
 		title = res[0].Result.FullTitle
 	} else {
-		menu := climenu.NewButtonMenu("Results for "+q, "Choose a song")
-
-		for i := 0; i < len(res); i++  {
-			menu.AddMenuItem(res[i].Result.FullTitle, strconv.Itoa(i))
+		info("Found results for \"" + q + "\":")
+		for i := 0; i < len(res); i++ {
+			fmt.Println(ctc.ForegroundBrightCyan, i, ctc.Reset, res[i].Result.FullTitle)
 		}
 
-		action, escaped := menu.Run()
-		if escaped {
-			os.Exit(0)
+		selected := -1
+
+		for selected < 0 || selected >= len(res) {
+			fmt.Print("\n > Choose a song [0]: ")
+			fmt.Scanln(&selected)
+			if (selected == -1) {
+				selected = 0
+			}
 		}
-		actionInt, _ := strconv.ParseInt(action, 0, 64)
-		url = res[actionInt].Result.URL
-		title = res[actionInt].Result.FullTitle
+
+
+		url = res[selected].Result.URL
+		title = res[selected].Result.FullTitle
+
+		info("Lyrics for "+title+" are being fetched...")
 	}
 
-	
 	l, _ := lyrics.LyricsURL(url)
-	lyrics := strings.ReplaceAll(fmt.Sprint(title,"\n",strings.Join(l, "\n")), "[", "\n[")
+	lyrics := strings.ReplaceAll(fmt.Sprint(title, "\n", strings.Join(l, "\n")), "[", "\n[")
 
 	if clipboard.Unsupported {
-		err("can't copy to the clipboard. Here are the lyrics:", false)
+		err("Can't copy to the clipboard. Here are the lyrics:", false)
 		if !noColors {
-			lyrics = strings.ReplaceAll(lyrics, "[", fmt.Sprint(ctc.ForegroundBrightYellow,"["))
-			lyrics = strings.ReplaceAll(lyrics, "]", fmt.Sprint("]",ctc.Reset))
+			lyrics = strings.ReplaceAll(lyrics, "[", fmt.Sprint(ctc.ForegroundBrightYellow, "["))
+			lyrics = strings.ReplaceAll(lyrics, "]", fmt.Sprint("]", ctc.Reset))
 		}
 		fmt.Println(lyrics)
 		os.Exit(1)
 	} else {
 		clipboard.WriteAll(lyrics)
-		if (printLyrics) {
+		if !(printLyrics && chooseFirst) {
+			success("Lyrics copied successfully!")
+			if printLyrics {
+				success("Printing lyrics")
+			}
+		}
+		if printLyrics {
 			if !noColors {
-				lyrics = strings.ReplaceAll(lyrics, "[", fmt.Sprint(ctc.ForegroundBrightYellow,"["))
-				lyrics = strings.ReplaceAll(lyrics, "]", fmt.Sprint("]",ctc.Reset))
+				lyrics = strings.ReplaceAll(lyrics, "[", fmt.Sprint(ctc.ForegroundBrightYellow, "["))
+				lyrics = strings.ReplaceAll(lyrics, "]", fmt.Sprint("]", ctc.Reset))
 			}
 			fmt.Println(lyrics)
-		} else {
-			fmt.Println(ctc.ForegroundGreen, "Lyrics written to clipboard successfully", ctc.Reset)
-		}
+		} 
+
 		os.Exit(0)
 	}
 
